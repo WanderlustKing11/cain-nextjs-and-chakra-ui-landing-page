97b1314adc943914df90ec7383e497f9d3ddfa90
Doug <wanderlustking11@gmail.com>
2023-11-08 14:54:40 -0500

    Add Chakra-UI Navbar; add Cain logo placeholder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <ChakraProvider>
-        <body className={inter.className}>{children}</body>
+        <Navbar />
+        {children}
       </ChakraProvider>
     </html>
   );
