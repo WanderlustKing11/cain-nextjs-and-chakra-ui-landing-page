b13846cc00652a2cf105466ad8ba0503e775f632
Doug <wanderlustking11@gmail.com>
2023-12-14 12:34:00 -0500

    Update ServiceCards style and responsiveness

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { Global } from '@emotion/react';
 import {
   Box,
   SimpleGrid,
@@ -9,10 +11,21 @@ import {
   Text,
   Button,
   Image,
+  transition,
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 
+const chevronAnimation = `
+  @keyframes chevronMove {
+    0% { transform: translateY(0); }
+    50% { transform: translateY(5px); }
+    100% { transform: translateY(0); }
+  }
+`;
+
 export default function ServiceCards() {
+  const [clicked, setClicked] = useState(new Array(3).fill(false));
+
   const cards = [
     {
       image: '/ps-10.jpg',
@@ -31,70 +44,110 @@ export default function ServiceCards() {
     },
   ];
 
+  const handleCardClick = (index: number) => {
+    const newClicked = [...clicked];
+    newClicked[index] = true;
+    setClicked(newClicked);
+    setTimeout(() => {
+      newClicked[index] = false;
+      setClicked(newClicked);
+    }, 400);
+  };
+
   return (
-    <Box      
-      mt={{ base: '375px', md: '200px'}} 
-      p={'10%'}
-      bg='black' 
-    >      
+    <>
+      <Global styles={chevronAnimation} />
+
+      <Box mt={{ base: '375px', md: '200px' }} p={'10%'} bg='black'>
         <SimpleGrid
-          spacing={4}
+          spacing={{ base: 16, md: 3, lg: 5, xl: 7 }}
           columns={{ base: 1, md: 3 }}
           justifyContent='center'
           alignItems='center'
         >
-            {cards.map((card, index) => (
-          <Card 
-            key={index} 
-            align='center'
-            w={{ base: '70%', sm: '80%', md: '90%', lg: '' }}
-            h={{ base: '20rem', sm: '30rem', md: '20rem', lg: '' }}
-            p={{ base: '4', sm: '8', md: '5' }}
-            borderRadius='15%'
-            bg={'blackAlpha.900'}
-            border={'4px'}
-            borderColor={'whiteAlpha.900'}
-            transition='background-color 0.3s ease-in-out'
-            _hover={{
-                bg: 'whiteAlpha.800',
-                cursor: 'pointer',
-                '.card-text': {
-                  color: 'black',
-                },
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              align='center'
+              onClick={() => handleCardClick(index)}
+              cursor='pointer'
+              w={{ base: '70%', sm: '80%', md: '90%', lg: '' }}
+              maxW={{ sm: '350px'}}
+              h={{
+                base: '20rem',
+                sm: '30rem',
+                md: '22rem',
+                lg: '22rem',
+                xl: '27rem',
+              }}
+              mx={'auto'}
+              p={{ base: 4, sm: 8, md: 5, xl: 7 }}
+              borderRadius='15%'
+              bg={'blackAlpha.900'}
+              border={'4px'}
+              borderColor={'whiteAlpha.900'}
+              transition='background-color 0.3s ease-in-out'
+              _hover={{
+                bg: 'red.900',
+                // '.card-text': {
+                //   color: 'black',
+                // },
+                // '.card-icon': {
+                //   color: 'black',
+                // },
+              }}
+              sx={{
                 '.card-icon': {
-                  color: 'black',
+                  animation: clicked[index]
+                    ? 'chevronMove 0.4s ease-in-out forwards'
+                    : 'none',
                 },
               }}
-          >
-            <Image
-              src={card.image}
-              alt={card.alt}
-              borderRadius='50%'
-              w={{ base: '100px', sm: '10rem', md: '80px', lg: '100px' }}
-              h={{ base: '100px', sm: '10rem', md: '80px', lg: '100px' }}
-            />
-            <CardBody>
-              <Text 
-                className='card-text'
-                align={'center'}
-                pt={{ sm: '10', md: '0'}}
-                fontSize={{ base: '1.3rem', sm: '1.5rem', md: '1.2rem' }}
-                color='whiteAlpha.900'
-              >
-                {card.text}
-              </Text>
-            </CardBody>
-            <CardFooter>
-              <ChevronDownIcon 
-                className='card-icon'
-                boxSize={10}  
-                color='whiteAlpha.900'
+            >
+              <Image
+                src={card.image}
+                alt={card.alt}
+                borderRadius='50%'
+                mt={{ base: 3, xl: 8 }}
+                w={{
+                  base: '90px',
+                  sm: '10rem',
+                  md: '80px',
+                  lg: '100px',
+                  xl: '130px',
+                }}
+                h={{
+                  base: '90px',
+                  sm: '10rem',
+                  md: '80px',
+                  lg: '100px',
+                  xl: '130px',
+                }}
+                objectFit='cover'
+                objectPosition={index === 2 ? 'right' : 'center' }
               />
-            </CardFooter>
-          </Card>      
-          ))}    
+              <CardBody>
+                <Text
+                  className='card-text'
+                  align={'center'}
+                  pt={{base: 3, sm: 5, md: 0, xl: 6 }}
+                  fontSize={{ base: '1rem', sm: '1.5rem', md: '1.2rem' }}
+                  color='whiteAlpha.900'
+                >
+                  {card.text}
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <ChevronDownIcon
+                  className='card-icon'
+                  boxSize={{ base: 10, sm: 20, md: 10, lg: 12 }}
+                  color='whiteAlpha.900'
+                />
+              </CardFooter>
+            </Card>
+          ))}
         </SimpleGrid>
-      
-    </Box>
+      </Box>
+    </>
   );
 }
